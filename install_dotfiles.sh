f5fce00ed797fd46385ca83fd3ba9a7a940d4769
Benny Wong <12177862+BennyW23@users.noreply.github.com>
2026-09-09 15:13:26 -0400

    add zsh plugin scripts

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,6 +1,20 @@
+echo 'linking dotfiles'
 for i in * .[^.]*; do
     file=${i##*/}
     if [ ${0##*/} != $file ] && [[ -f "./$file" ]]; then
         ln -s ~/dotfiles/$file ~/$file
     fi
 done
+
+echo
+echo 'installing spaceship'
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+echo
+echo 'installing zsh-autosuggestions'
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+echo
+echo 'installing zsh-syntax-highlighting'
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
